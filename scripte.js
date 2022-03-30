0d9d6a663916314cedfff95838f728a43634c881
DIOUF-MOR <daaroukhoudos04@gmail.com>
2022-03-30 19:59:57 +0000

    second commit

--- scripte.js
+++ scripte.js
@@ -11,8 +11,8 @@ addColom.addEventListener('click', function () {
     if (contener.childElementCount < 5) {
         i++
         contener.appendChild(creatColonne());
+        refreche()
     }
-
 
 })
 addNote.addEventListener('click', function () {
@@ -24,6 +24,7 @@ addNote.addEventListener('click', function () {
         addNote.style.border = 'red 2px solid'
     }
 })
+
 function creatFormulaire() {
     const contentFormulaire = document.createElement('div')
     contentFormulaire.className = "contentFormulaire"
@@ -48,6 +49,7 @@ function creatFormulaire() {
     divtacheLabel.appendChild(tacheLabel)
     const textarea = document.createElement('textarea')
     textarea.setAttribute('id', 'textarea')
+    textarea.setAttribute('class', 'textarea')
     const divdateLabel = document.createElement('div')
     divdateLabel.className = "divLabel"
     const dateLabel = document.createElement('label')
@@ -55,6 +57,7 @@ function creatFormulaire() {
     divdateLabel.appendChild(dateLabel)
     const inputDate = document.createElement('input'); inputDate.type = 'date'
     inputDate.setAttribute('id', 'inputDate')
+    inputDate.setAttribute('class', 'inputDate')
     const divtimeLabel = document.createElement('div')
     divtimeLabel.className = "divLabel"
     const timeLabel = document.createElement('label')
@@ -62,6 +65,7 @@ function creatFormulaire() {
     divtimeLabel.appendChild(timeLabel)
     const inputTime = document.createElement('input'); inputTime.type = 'time'
     inputTime.setAttribute('id', 'inputTime')
+    inputTime.setAttribute('class', 'inputTime')
     const divtimefinLabel = document.createElement('div')
     divtimefinLabel.className = "divLabel"
     const timefinLabel = document.createElement('label')
@@ -69,12 +73,15 @@ function creatFormulaire() {
     divtimefinLabel.appendChild(timefinLabel)
     const inputTimefin = document.createElement('input'); inputTimefin.type = 'time'
     inputTimefin.setAttribute('id', 'inputTimefin')
+    inputTimefin.setAttribute('class', 'inputTimefin')
     const button = document.createElement('button')
     button.className = "ajout"
     button.innerText = "Ajoute"
     button.addEventListener('click', function () {
-        let note = document.querySelector('.contentNote')
-        note.appendChild(contentTache())
+        const note = document.querySelector('.contentNote');
+        const divTache = document.createElement('div');
+        divTache.className = "divTache";
+        note.appendChild(recupereNote(divTache));
         contentFormulaire.classList.add('animate__animated', 'animate__zoomOutLeft')
     })
     corpFormulair.append(divtacheLabel, textarea, divdateLabel, inputDate, divtimeLabel, inputTime, divtimefinLabel, inputTimefin, button)
@@ -82,11 +89,15 @@ function creatFormulaire() {
     contentFormulaire.appendChild(formulaire)
     return contentFormulaire;
 }
-function creatColonne() {
 
+let compt = 1;
+function creatColonne() {
+    const contener = document.querySelector('.contener')
     const collone = document.createElement('div')
     collone.className = "collone"
-    collone.setAttribute('id',i)
+    collone.id = compt;
+    /* console.log(collone.id); */
+    compt++;
 
     const titre = document.createElement('span')
     titre.className = "titre animate__animated animate__zoomIn"
@@ -94,20 +105,20 @@ function creatColonne() {
         titre.contentEditable = true
     })
     const h3 = document.createElement('h3')
-    h3.innerHTML = "Colonne " + i
-
+    h3.className = 'titre-h3';
+    h3.innerText = "Colonne " + i
     if (i > 5) {
         i = 0;
     }
     const croix = document.createElement('i')
-    croix.className = "fa-solid fa-delete-left"
-    croix.addEventListener('click', function () {
+    croix.className = "fa-solid fa-delete-left";
+    croix.addEventListener('click', function (e) {
+        if (e.target.parentElement.parentElement != contener.children[0] || contener.childElementCount == 1) {
 
+            conteneurPrincipal.appendChild(modalSuprimer(e.target.parentElement.parentElement));
+            // refreche();
 
-        conteneurPrincipal.appendChild(modalSuprimer())
-
-
-
+        }
     })
 
     const contentNote = document.createElement('span');
@@ -116,41 +127,71 @@ function creatColonne() {
     contentNote.style.backgroundColor = colors[i]
 
     titre.append(h3, croix)
+    // refreche()
     collone.append(titre, contentNote);
 
     return collone
 }
-function contentTache() {
-    const divTache = document.createElement('div');
-    divTache.className = "divTache";
+
+function recupereNote(parent) {
+
     const buttonG = document.createElement('button');
     buttonG.className = "btn";
     buttonG.id = "buttonG";
     buttonG.innerHTML = "&#xab";
-
+    buttonG.addEventListener('click', function (e) {
+        collone = e.target.parentElement.parentElement.parentElement;
+        const idColloneActuel = +collone.id;
+        const collonePrecedent = document.getElementById(idColloneActuel - 1);
+        collonePrecedent.lastChild.appendChild(e.target.parentElement)
+    })
     const buttonD = document.createElement('button');
     buttonD.className = "btn";
     buttonD.id = "buttonD";
     buttonD.innerHTML = "&#xbb";
-    buttonD.addEventListener('click',function(){
-        this.parentElement.parentElement.parentElement.parentElement.children[id.target+1].lastElementChild.appendChild(this.parentElement)
+    buttonD.addEventListener('click', function (e) {
+        collone = e.target.parentElement.parentElement.parentElement;
+        const idColloneActuel = +collone.id;
+        const colloneSuivant = document.getElementById(idColloneActuel + 1);
+        colloneSuivant.lastChild.appendChild(e.target.parentElement)
     })
-    const valeurTache = document.createElement('span')
-    valeurTache.setAttribute('id', 'valeurTache')
-    recupereNote(valeurTache);
-    divTache.append(buttonG, valeurTache, buttonD);
-    return divTache
-}
-function recupereNote(parent) {
-    const textarea = document.querySelector('#textarea').value;
-    const inputDate = document.querySelector('#inputDate').value;
-    const inputTime = document.querySelector('#inputTime').value;
-    const inputTimefin = document.querySelector('#inputTimefin').value;
+    const valeurTache = document.createElement('div');
+    valeurTache.setAttribute('id', 'valeurTache');
 
-    parent.append(textarea, inputDate, inputTime, inputTimefin);
+    const contentTexarea = document.createElement('span');
+    const textarea = document.querySelectorAll('.textarea');
+    for (let i = 0; i < textarea.length; i++) {
+        var valTextArea = "";
+        valTextArea = textarea[i].value
+        contentTexarea.innerText = valTextArea;
+    }
+    const contentInputDate = document.createElement('span');
+    const inputDate = document.querySelectorAll('.inputDate');
+    for (let i = 0; i < inputDate.length; i++) {
+        var valinputDate = "";
+        valinputDate = inputDate[i].value
+        contentInputDate.innerText = valinputDate;
+    }
+    const contentInputTime = document.createElement('span');
+    const inputTime = document.querySelectorAll('.inputTime');
+    for (let i = 0; i < inputTime.length; i++) {
+        var valinputTime = ""
+        valinputTime = inputTime[i].value
+        contentInputTime.innerText = valinputTime;
+    }
+    const contentInputTimefin = document.createElement('span');
+    const inputTimefin = document.querySelectorAll('.inputTimefin');
+    for (let i = 0; i < inputTimefin.length; i++) {
+        var valinputTimefin = ""
+        valinputTimefin = inputTimefin[i].value
+        contentInputTimefin.innerText = valinputTimefin;
+    }
+    valeurTache.append(contentTexarea, contentInputDate, contentInputTime, contentInputTimefin);
+    parent.append(buttonG, valeurTache, buttonD)
     return parent;
 }
-function modalSuprimer() {
+
+function modalSuprimer(parent) {
 
     const collone = document.querySelector('.collone');
     const modal = document.createElement('div');
@@ -163,24 +204,26 @@ function modalSuprimer() {
 
     const p = document.createElement('p');
     p.setAttribute('class', 'msgModal');
-    p.innerText = 'Etees-vous vraiment sur de vouloir suprimer cette colonne avec ses taches';
+    p.innerText = 'Etes-vous vraiment sur de vouloir suprimer cette colonne avec toutes ses taches';
 
     const suprimer = document.createElement('button');
-    suprimer.innerHTML = "Suprimer";
+    suprimer.innerText = "Suprimer";
     suprimer.setAttribute('id', 'suprimer');
     suprimer.setAttribute('class', 'btnModal');
     suprimer.addEventListener('click', function () {
-        // this.parentElement.parentElement.remove()
-        collone.target.remove()
+        parent.remove()
         modal.classList.add('animate__animated', 'animate__zoomOutLeft')
+
+        refreche();
     })
+
     const annuler = document.createElement('button');
     annuler.innerHTML = "Annuler";
     annuler.setAttribute('id', 'annuler');
     annuler.setAttribute('class', 'btnModal');
     annuler.addEventListener('click', function () {
         modal.classList.add('animate__animated', 'animate__zoomOutLeft')
-        collone.remove();
+        // collone.remove();
     })
     const divbtn = document.createElement('div');
     divbtn.setAttribute('class', 'divbtn');
@@ -190,8 +233,16 @@ function modalSuprimer() {
     modalSpr.append(p, divbtn);
 
     modal.appendChild(modalSpr)
-
     return modal;
+}
+
+function refreche() {
+    const titreH3 = document.querySelectorAll('.titre-h3');
+    console.log(titreH3.length)
+    titreH3.forEach((element, i) => {
+        element.innerText = 'Colonne' + (i + 1);
+        element.parentElement.parentElement.id = i + 1;
+    })
 }
 
 
